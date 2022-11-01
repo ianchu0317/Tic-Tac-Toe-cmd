c22665794a3ea7da5d706eace6161ffe42ea649b
Ian Chen <71509578+ianchu0317@users.noreply.github.com>
2022-11-01 13:24:35 -0300

    Created table showing with nested loop and nested array, but need to change method to change table

--- Tic-Tac-Toe-cmd.py
+++ Tic-Tac-Toe-cmd.py
@@ -1,5 +1,3 @@
-from time import sleep
-import pprint
 import sys
 
 """ expectative: 
@@ -15,19 +13,26 @@ import sys
 
 
 class Table:
+    # Declarations
     available_positions = {(x, y): True for x in range(1, 4) for y in range(1, 4)}
-    p1_selected = []
-    p2_selected = []
+    table = [[" ", " ", " "],
+             [" ", " ", " "],
+             [" ", " ", " "]]   # Create table as an array so later can use join() to print as string in a nested loop
+    p1_selected, p2_selected = list(), list()
     player_turn = "p1"
     players = ["p1", "p2"]
+    player_symbol = {"p1": "X", "p2": "0"}
     PLAYING = True
+
+    def __show_info(self):
+        print(self.available_positions, self.players, self.p1_selected, self.p2_selected)
 
     def check_position(self, p):
         try:
             if self.available_positions[p]:
-                self.available_positions[p] = False  # Change availability
+                self.available_positions[p] = False  # Change availability of selected position
 
-                if self.player_turn == "p1":    # Add to done list
+                if self.player_turn == "p1":    # Add to player's selections
                     self.p1_selected.append(p)
                 else:
                     self.p2_selected.append(p)
@@ -44,16 +49,28 @@ class Table:
     def check_win(self):
         pass
 
-    def show_table(self):
-        print("""
-             1   2   3
+    def update_table(self, position):
+        self.table[position[0]-1][position[1]-1] = self.player_symbol.get(self.player_turn)
 
-        1    0 | x | x 
-            ---+---+---
-        2    X | 0 | X
-            ---+---+---
-        3      |   | 
-        """)
+    def show_table(self):
+        # Format output and show table
+        rc = cc = 2  # row and column counter
+        _cc = 1
+        print("    1   2   3")
+        for column in self.table:
+            for row in column:
+                if rc == 2:
+                    print(f"{_cc}", end='')
+                    print("  ", end='')
+                print(f" {row} ", end='')
+                if rc:
+                    print("|", end='')
+                    rc -= 1
+            rc = 2
+            _cc += 1
+            if cc:
+                print("\n   ---+---+---")
+                cc -= 1
 
     def error(self):
         print("[!] Enter a valid option... ")
@@ -61,17 +78,21 @@ class Table:
 
     def enter_value(self):
         try:
-            print("Player {} turn".format(self.player_turn))
+            print("\nPlayer {} turn".format(self.player_turn))
             position = tuple(map(lambda x: int(x), input("Enter coordinates x y (i.e 2 3): ").split(" ")))
             self.check_position(position)
+            return position
         except Exception as e:
             print(e)
             self.error()
 
     def main(self):
         while self.PLAYING:
+            self.__show_info()
             self.show_table()
-            self.enter_value()
+            position = self.enter_value()
+            self.update_table(position)
+        sys.exit()
 
 
 if __name__ == '__main__':
